51dc77bb451e3db27b19f3af31854f7bec15a4d1
Susan <susan.huang@tenfoldtech.com>
2019-10-15 10:19:47 +0800

    [add] 新增資源頁面

--- assets/themes/susan/js/index.js
+++ assets/themes/susan/js/index.js
@@ -1,18 +1,60 @@
-(function() {
+(function () {
   if (window.location.pathname === '/') {
     $('.nav-list li')
       .first()
-      .addClass('active');
+      .addClass('active')
   } else {
     $('.nav-list li')
       .first()
-      .removeClass('active');
+      .removeClass('active')
   }
-  var md = window.markdownit();
-  var result = md.render('# markdown-it rulezz!');
-  $('#nav-main .menu-icon').on('click', function() {
-    $('#nav-main').toggleClass('active');
-  });
+  var md = window.markdownit()
+  var result = md.render('# markdown-it rulezz!')
+  $('#nav-main .menu-icon').on('click', function () {
+    $('#nav-main').toggleClass('active')
+  })
+  var $resources = $('#resources')
+  if ($resources) {
+    var resourcesInfo = ''
+    var resourcesList = [
+      {
+        title: '字蛛 ( FontSpider )',
+        text: '字蛛是一个中文字体压缩器',
+        url: 'http://font-spider.org/',
+        image: '/assets/themes/susan/images/FontSpider.png'
+      },
+      {
+        title: 'Can I use...',
+        text: 'Browser support tables for modern web technologies',
+        url: 'https://caniuse.com',
+        image: '/assets/themes/susan/images/CanIUse.png'
+      },
+      {
+        title: 'Normalize.css',
+        text: 'A modern, HTML5-ready alternative to CSS resets',
+        url: 'https://necolas.github.io/normalize.css/',
+        image: '/assets/themes/susan/images/Normalize.png'
+      },
+      {
+        title: '日本の伝統色',
+        text: '日本傳統顏色',
+        url: 'http://nipponcolors.com/',
+        image: '/assets/themes/susan/images/NIPPONCOLORS.png'
+      },
+      {
+        title: 'FANCY-BORDER-RADIUS',
+        text: '漂亮的圓角設定',
+        url: 'https://9elements.github.io/fancy-border-radius/',
+        image: '/assets/themes/susan/images/FancyBorderRadius.png'
+      }
+    ]
+    for (var i = 0; i < resourcesList.length; i++) {
+      var resourcesMode = '<div class="col-3"><a href="'.concat(resourcesList[i].url, '" target="_blank"><div class="card"><div class="card-image" style="background-image:url(').concat(resourcesList[i].image, ');"></div><div class="card-info"><div class="title">').concat(resourcesList[i].title, '</div><div class="text">').concat(resourcesList[i].text, '</div></div></div></a></div>')
+      resourcesInfo += resourcesMode
+    }
+    $('#resources').html(resourcesInfo)
+  }
+
   // $('#nav-main').on('click', function () {
   //   $('#nav-main').toggleClass('active');
   // }
@@ -39,4 +81,4 @@
   //     console.log(data)
   //   }
   // )
-})();
+})()
